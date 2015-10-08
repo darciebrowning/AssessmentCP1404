dbb66eac637f84b8c8135e1185396c89f696eab3
Darcie Browning <darcie.browning@my.jcu.edu.au>
2015-10-08 14:00:11 +1000

    Details class as well as is empty.

--- currency.py
+++ currency.py
@@ -2,17 +2,29 @@ __author__ = 'Darcie'
 import web_utility
 from trip import *
 
+
 def convert(amount, home_currency_code, location_currency_code):
-    """This function generates the URL string for the contries the user wishes to convert and returns the unformatted version of the conversion"""
+    """This function generates the URL string for the countries the user
+    wishes to convert and returns the un-formatted version of the conversion"""
 
-    url_string = ("https://www.google.com/finance/converter?a= {} &from= {}  &to= {}".format(amount, home_currency_code, location_currency_code))
-    result = web_utility.load_page(url_string)
-    result = (result[result.index('result'):])
-    #TODO put in exception to equal - 1
+    if home_currency_code != location_currency_code:
+        try:
+            url_string = ("https://www.google.com/finance/converter?a= {} &from= {}  &to= {}".format(amount, home_currency_code, location_currency_code))
+            result = web_utility.load_page(url_string)
+            result = (result[result.index('result'):])
 
-    split_result = result.split(">") [2]
-    amount = split_result.split(" ") [0]
-    return amount
+            split_result = result.split(">")[2]
+            amount = split_result.split(" ")[0]
+            return amount
+
+        # Exception if website is unreachable.
+        except FileNotFoundError:
+            amount = -1
+            return amount
+    else:
+        # occurs if location_currency_code and home_currency_code are the same values
+        amount = -1
+        return amount
 
 
 def get_details(country_name):
@@ -31,24 +43,26 @@ def get_details(country_name):
 
     #close the file
     file.close()
-    return ()
+
+    # If the country name isn't found in the file return an empty tuple.
+    return tuple()
 
 
-# Get details for the users home country
-country_name = input("Enter your Country name: ")
-home_currency_code = get_details(country_name) [1]
-
-
-# Get details for the country the visitor is converting to
-country_name = input("Enter the country you would like to convert to: ")
-location_currency_code = get_details(country_name) [1]
-
-
-# Get the amount the user would like to convert
-amount = input("Enter the value you would like to convert: ")
-
-# pass values to the convert function and run it
-convert(amount, home_currency_code, location_currency_code)
-
-# Pass amount to format currency to be formatted.
-Country.format_currency(amount)
+# # Get details for the users home country
+# country_name = input("Enter your Country name: ")
+# home_currency_code = get_details(country_name) [1]
+#
+#
+# # Get details for the country the visitor is converting to
+# country_name = input("Enter the country you would like to convert to: ")
+# location_currency_code = get_details(country_name) [1]
+#
+#
+# # Get the amount the user would like to convert
+# amount = input("Enter the value you would like to convert: ")
+#
+# # pass values to the convert function and run it
+# #convert(amount, home_currency_code, location_currency_code)
+#
+# # Pass amount to format currency to be formatted.
+# #Country.format_currency(Country.self, amount)
--- trip.py
+++ trip.py
@@ -1,10 +1,9 @@
 __author__ = 'Darcie'
-from currency import *
+import currency
+import datetime
 
 
 class Error(Exception):
-
-
     pass
 
 
@@ -15,17 +14,40 @@ class Country:
         self.currency_code = currency_code
         self.currency_symbol = currency_symbol
 
-    def format_currency(amount):
+    def format_currency(self, amount):
+        amount = '{1:.2f}'.format(amount)
+        str(amount)
+        return amount
 
-
+    def __str__(self, details):
         pass
 
 class Details:
 
-    def __init__(self, location):
-        self.location = location
+    def __init__(self, locations):
+        """Create a list for the locations visited to be stored in."""
+        self.locations = []
 
-    pass
+    def add(self, country_name, start_date, end_date):
+
+        start_date = "{}/{}/{}". format(start_date[0], start_date[1], start_date[2])
+        end_date = "{}/{}/{}".format(end_date[0], end_date[1], end_date[2])
+
+        self.locations.append((country_name, start_date, end_date))
+
+    def current_country(self, date_string):
+
+        for location in self.locations:
+            if date_string >= location[1] and date_string <= location[2]:
+                return location[0]
+
+    def is_empty(self):
+
+        if self.locations[0] == []:
+            return False
+        else:
+            return True
 
 
 
+date_string = datetime.date.today().strftime("%Y/%m/%d")
